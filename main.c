219eb323f36652543fe784787c7e33c13811211d
FelipeAlvarezEscobar <137942137+FelipeAlvarezEscobar@users.noreply.github.com>
2023-09-17 19:11:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_PUNTOS 100
+#define MAX_TURISTAS 100
+
+typedef struct {
+    char nombre[100];
+    char tipo[50];
+    char direccion[200];
+    char horario[100];
+    char descripcion[500];
+} PuntoDeInteres;
+
+typedef struct {
+    char pasaporte[20];
+    char nombre[100];
+    char pais[50];
+    char lugaresFavoritos[10][100];
+    int numLugaresFavoritos;
+} Turista;
+
+PuntoDeInteres puntosDeInteres[MAX_PUNTOS];
+int numPuntosDeInteres = 0;
+
+Turista turistas[MAX_TURISTAS];
+int numTuristas = 0;
+
+void registrarPuntoDeInteres(char* nombre, char* tipo, char* direccion, char* horario, char* descripcion);
+void mostrarDatosPuntoDeInteres(char* nombre);
+void eliminarPuntoDeInteres(char* nombre);
+void registrarTurista(char* pasaporte, char* nombre, char* pais);
+void agregarLugarFavoritoATurista(char* pasaporte, char* nombreLugar);
+void mostrarTuristasPorPais(char* pais);
+void mostrarPuntosDeInteresPorTipo(char* tipo);
+void importarDesdeCSV(char* nombreArchivoPuntos, char* nombreArchivoTuristas);
+void exportarACSV(char* nombreArchivoPuntos, char* nombreArchivoTuristas);
+
+int main() {
+    int opcion;
+    char nombre[100];
+    char tipo[50];
+    char direccion[200];
+    char horario[100];
+    char descripcion[500];
+    char pasaporte[20];
+    char nombreLugar[100];
+    char pais[50];
+    char tipoDeLugar[50];
+    char nombreArchivoPuntos[100];
+    char nombreArchivoTuristas[100];
+
+    // Menú de la aplicación
+    while (1) {
+        printf("\n********** Aplicación de Gestión Turística **********\n");
+        printf("1. Registrar Punto de Interés\n");
+        printf("2. Mostrar Datos de Punto de Interés\n");
+        printf("3. Eliminar Punto de Interés\n");
+        printf("4. Registrar Turista\n");
+        printf("5. Agregar Lugar Favorito a Turista\n");
+        printf("6. Mostrar Turistas por País\n");
+        printf("7. Mostrar Puntos de Interés por Tipo\n");
+        printf("8. Importar desde Archivos CSV\n");
+        printf("9. Exportar a Archivos CSV\n");
+        printf("0. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                // Llamada a la función registrarPuntoDeInteres
+                break;
+            case 2:
+                // Llamada a la función mostrarDatosPuntoDeInteres
+                break;
+            case 3:
+                // Llamada a la función eliminarPuntoDeInteres
+                break;
+            case 4:
+                // Llamada a la función registrarTurista
+                break;
+            case 5:
+                // Llamada a la función agregarLugarFavoritoATurista
+                break;
+            case 6:
+                // Llamada a la función mostrarTuristasPorPais
+                break;
+            case 7:
+                // Llamada a la función mostrarPuntosDeInteresPorTipo
+                break;
+            case 8:
+                // Llamada a la función importarDesdeCSV
+                break;
+            case 9:
+                // Llamada a la función exportarACSV
+                break;
+            case 0:
+                printf("Saliendo de la aplicación. ¡Hasta luego!\n");
+                exit(0);
+            default:
+                printf("Opción no válida. Por favor, seleccione una opción válida.\n");
+        }
+    }
+
+    return 0;
+}
+
+// Implementación de las funciones
+// Debes completar la implementación de estas funciones según tus necesidades.
+void registrarPuntoDeInteres(char* nombre, char* tipo, char* direccion, char* horario, char* descripcion) {
+    // Implementación aquí
+}
+
+void mostrarDatosPuntoDeInteres(char* nombre) {
+    // Implementación aquí
+}
+
+void eliminarPuntoDeInteres(char* nombre) {
+    // Implementación aquí
+}
+
+void registrarTurista(char* pasaporte, char* nombre, char* pais) {
+    // Implementación aquí
+}
+
+void agregarLugarFavoritoATurista(char* pasaporte, char* nombreLugar) {
+    // Implementación aquí
+}
+
+void mostrarTuristasPorPais(char* pais) {
+    // Implementación aquí
+}
+
+void mostrarPuntosDeInteresPorTipo(char* tipo) {
+    // Implementación aquí
+}
+
+void importarDesdeCSV(char* nombreArchivoPuntos, char* nombreArchivoTuristas) {
+    FILE *archivoPuntos = fopen(nombreArchivoPuntos, "r");
+    FILE *archivoTuristas = fopen(nombreArchivoTuristas, "r");
+
+    if (archivoPuntos == NULL || archivoTuristas == NULL) {
+        printf("Error al abrir uno o ambos archivos CSV.\n");
+        return;
+    }
+
+    // Leer puntos de interés desde el archivo CSV
+    char linea[1000];
+    while (fgets(linea, sizeof(linea), archivoPuntos)) {
+        char *token = strtok(linea, ",");
+        if (token != NULL) {
+            strcpy(puntosDeInteres[numPuntosDeInteres].nombre, token);
+            token = strtok(NULL, ",");
+            strcpy(puntosDeInteres[numPuntosDeInteres].tipo, token);
+            token = strtok(NULL, ",");
+            strcpy(puntosDeInteres[numPuntosDeInteres].direccion, token);
+            token = strtok(NULL, ",");
+            strcpy(puntosDeInteres[numPuntosDeInteres].horario, token);
+            token = strtok(NULL, ",");
+            strcpy(puntosDeInteres[numPuntosDeInteres].descripcion, token);
+            numPuntosDeInteres++;
+        }
+    }
+
+    // Leer turistas desde el archivo CSV
+    while (fgets(linea, sizeof(linea), archivoTuristas)) {
+        char *token = strtok(linea, ",");
+        if (token != NULL) {
+            strcpy(turistas[numTuristas].pasaporte, token);
+            token = strtok(NULL, ",");
+            strcpy(turistas[numTuristas].nombre, token);
+            token = strtok(NULL, ",");
+            strcpy(turistas[numTuristas].pais, token);
+            token = strtok(NULL, ",");
+            char *lugaresFavoritos = strtok(token, ";");
+            while (lugaresFavoritos != NULL && turistas[numTuristas].numLugaresFavoritos < 10) {
+                strcpy(turistas[numTuristas].lugaresFavoritos[turistas[numTuristas].numLugaresFavoritos], lugaresFavoritos);
+                turistas[numTuristas].numLugaresFavoritos++;
+                lugaresFavoritos = strtok(NULL, ";");
+            }
+            numTuristas++;
+        }
+    }
+
+    fclose(archivoPuntos);
+    fclose(archivoTuristas);
+
+    printf("Datos importados desde archivos CSV.\n");
+}
+
+void exportarACSV(char* nombreArchivoPuntos, char* nombreArchivoTuristas) {
+    // Implementación aquí
+}
